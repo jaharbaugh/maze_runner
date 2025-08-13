fe0636ec40e21570a730a0fe84a601e016635dfb
jaharbaugh <harbaugh.ja@gmail.com>
2025-08-13 07:33:06 -0500

    Finished maze solving algorithm

--- maze.py
+++ maze.py
@@ -102,3 +102,58 @@ class Maze():
             for cell in row:
                 cell.visited=False
 
+    def solve(self):
+        return self._solve_r(0,0)
+        
+    
+    def _solve_r(self, i, j):
+        self._animate()
+        self.__cells[i][j].visited=True
+
+        if i == self.num_rows-1 and j == self.num_cols-1:
+            return True
+        
+        if (i-1 >=0 and
+            self.__cells[i][j].has_top_wall==False and
+            self.__cells[i-1][j].visited==False):
+            
+            self.__cells[i][j].draw_move(self.__cells[i-1][j])
+
+            if self._solve_r(i-1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i-1][j], True)
+        
+        if (j-1 >=0 and
+            self.__cells[i][j].has_left_wall==False and
+            self.__cells[i][j-1].visited==False):
+            
+            self.__cells[i][j].draw_move(self.__cells[i][j-1])
+
+            if self._solve_r(i, j-1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j-1], True)
+        if (j+1 <= self.num_cols -1 and
+            self.__cells[i][j].has_right_wall==False and
+            self.__cells[i][j+1].visited==False):
+            
+            self.__cells[i][j].draw_move(self.__cells[i][j+1])
+
+            if self._solve_r(i, j+1):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i][j+1], True)
+        if (i+1 <= self.num_rows -1 and
+            self.__cells[i][j].has_bottom_wall==False and
+            self.__cells[i+1][j].visited==False):
+            
+            self.__cells[i][j].draw_move(self.__cells[i+1][j])
+
+            if self._solve_r(i+1, j):
+                return True
+            else:
+                self.__cells[i][j].draw_move(self.__cells[i+1][j], True)
+        
+
+        return False
